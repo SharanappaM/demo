fe5cf27dc18f65d1c50f30fd562db7086482c9f8
Sharanappa <sharanappa@nextiqz.com>
2025-09-26 23:44:47 +0530

    added button comp

---
+++ src/componets/customComponets/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({title, disabled}) => {
+  return (
+    <button disabled={disabled}>{title}</button>
+  )
+}
+
+export default Button
--- src/componets/customComponets/Card.jsx
+++ src/componets/customComponets/Card.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Button from './Button'
 
 const Card = ({titel, des}) => {
   return (
     <div style={{border:2, borderColor:"pink", backgroundColor:"grey"}}>
         <h1 style={{color:"red"}}>{titel}</h1>
         <p>{des}</p>
+
+        <Button title="Save user details" disabled={false}/>
       
     </div>
   )
