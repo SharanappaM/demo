17e37770a872db75bf8a91a059770693b1442c74
Sharanappa <sharanappa@nextiqz.com>
2025-09-26 23:53:25 +0530

    commit added button to card comp

--- src/componets/customComponets/Card.jsx
+++ src/componets/customComponets/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({titel, des}) => {
     <div style={{border:2, borderColor:"pink", backgroundColor:"grey", padding:10, borderRadius:10}}>
         <h1 style={{color:"red" }}>{titel}</h1>
         <p style={{padding:5}}>{des}</p>
-
+<Button/>
       
     </div>
   )
