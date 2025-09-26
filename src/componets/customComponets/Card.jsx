a1292219fcb6ca4878a39ebd43a2353806caa3c4
Sharanappa <sharanappa@nextiqz.com>
2025-09-27 00:06:37 +0530

    Revert "removed card content"
    This reverts commit d1c00a1fad0c74368d0f3ae5c34d4850ea74c98b.

--- src/componets/customComponets/Card.jsx
+++ src/componets/customComponets/Card.jsx
@@ -4,7 +4,12 @@ import Button from './Button'
 const Card = ({titel, des}) => {
   return (
 
-<div></div>
+    <div style={{border:2, borderColor:"pink", backgroundColor:"grey", padding:10, borderRadius:10}}>
+        <h1 style={{color:"red" }}>{titel}</h1>
+        <p style={{padding:5}}>{des}</p>
+<Button/>
+      
+    </div>
   )
 }
 
